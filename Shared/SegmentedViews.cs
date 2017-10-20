198b41a5924b2748b373ee8acaabfde68609c462
Siavash <siavash.mohseni@geeks.ltd.uk>
2017-10-20 15:07:56 +0100

    Initial commit

---
+++ Shared/SegmentedViews.cs
@@ -0,0 +1,73 @@
+﻿namespace Zebble
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public partial class SegmentedViews : Stack
+    {
+        public TimeSpan AnimationDuration = 300.Milliseconds();
+        public AnimationEasing Easing = AnimationEasing.EaseInOut;
+        public SegmentedViews() { ClipChildren = false; }
+    }
+
+    public class Segments : Stack
+    {
+        public Segment[] Items => this.CurrentChildren<Segment>().ToArray();
+        public Segments() : base(RepeatDirection.Horizontal) { Id = "SegmentsContainer"; }
+    }
+
+    public class Segment : Button
+    {
+        public int? Index => Container?.CurrentChildren<Segment>()?.IndexOf(this);
+        public Segments Container => FindParent<Segments>();
+        public SegmentedViews RootView => Container.FindParent<SegmentedViews>();
+        public override async Task OnInitializing()
+        {
+            await base.OnInitializing();
+
+            Tapped.Handle(async () =>
+            {
+                if (Container == null || Index == null)
+                {
+                    Device.Log.Error("Each Segment should be inside a Segments view.");
+                    return;
+                }
+
+                if (Root == null)
+                {
+                    Device.Log.Error("Segments should be inside a SegmentedViews view.");
+                    return;
+                }
+
+                Container.Items.Do(async (b) => await b.UnsetPseudoCssState("active"));
+                await SetPseudoCssState("active");
+
+                var contents = Container.CurrentSiblings<Contents>().FirstOrDefault();
+
+                if (contents == null)
+                {
+                    Device.Log.Error("Each content should be inside a Contents view.");
+                    return;
+                }
+
+                await contents.Animate(
+                    RootView.AnimationDuration,
+                    RootView.Easing,
+                    (c) => c.X(-c.Items[Index.Value].X.CurrentValue));
+            });
+        }
+    }
+
+    public class Contents : Stack
+    {
+        public Content[] Items => this.CurrentChildren<Content>().ToArray();
+        public Contents() : base(RepeatDirection.Horizontal) { Id = "ViewsContainer"; ClipChildren = false; }
+    }
+
+    public class Content : Stack
+    {
+        public int? Index => Container?.CurrentChildren<Content>()?.IndexOf(this);
+        public Contents Container => FindParent<Contents>();
+    }
+}
